556c85a3d3566bde45e333f73813f50f3e393e97
tsam137 <tobasamuel137@gmail.com>
2019-03-25 12:08:23 +0000

    Hubspot Project

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,16 @@
-
-function myFunction() {
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // jQuery can do a lot of crazy stuff so make sure to google around to find out more
-
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
-
-  // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
-}
-
+  // Gets called whenever the user clicks "sign in" or "sign out".
+  function toggleSignIn() {
+    if (!firebase.auth().currentUser) { // if the user's not logged in, handle login
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/plus.login');
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log("success");
+      }).catch(function(error) {
+        console.error("error", error);
+      });
+    } else { // handle logout
+      firebase.auth().signOut();
+    }
+    //This disables the button until login or logout is successful. You'll want to replace 'login-button' with the id of your login button.
+    $('#login-button').attr("disabled", true);
+  }
